8b251e500f06aa57198c9b1f23eab96e785b0482
Shopping <xpzheng6@163.com>
2023-03-14 20:54:13 +0800

    feat: update web app

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -3,23 +3,39 @@ import { ref } from 'vue'
 import service from './service'
 
 const msg = ref('')
+const count = ref('')
+const mysql = ref('')
 
 async function load() {
   const hello = (await service.get('/')).data
-  const count = (await service.get('/count')).data
-  msg.value = `${hello}, ${count}`
+  msg.value = `${hello}`
+}
+
+async function inc() {
+  const { data } = await service.get('/count')
+  count.value = data
+}
+
+async function loadDb() {
+  const { data } = await service.get('/db')
+  mysql.value = data
 }
 
 load()
-
-service.get('/').then(res => {
-  msg.value = res.data
-})
+inc()
+loadDb()
 </script>
 
 <template>
-  <div class="msg">
-    {{ msg }}
+  <div style="text-align: center;">
+    <div class="msg">
+      {{ msg }}
+    </div>
+    <div style="margin-top: 10px;">
+      <button @click="inc">Redis +{{ count }}</button>
+    </div>
+
+    <div style="margin-top: 10px;">From Mysql: {{ mysql }}</div>
   </div>
 </template>
 
